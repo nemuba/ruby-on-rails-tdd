36a963904e8b0d753428e7d5c700b98229078f26
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-08-31 22:20:57 -0300

    subject

--- entendendo_rspec/spec/calculator/calculator_spec.rb
+++ entendendo_rspec/spec/calculator/calculator_spec.rb
@@ -2,42 +2,52 @@ require 'calculator'
 
 describe Calculator do
 
-  subject { Calculator.new } 
-
-  it "#sum for 2 numbers positive" do
-    expect(subject.sum(5,7)).to eq(12)
+  context "#method - sum" do
+    it "with 2 numbers positive" do
+      expect(subject.sum(5,7)).to eq(12)
+    end
+  
+    it "with 2 numbers negative" do
+      expect(subject.sum(-5,-7)).to eq(-12)
+    end
   end
+  
 
-  it "#sum for 2 numbers negative" do
-    expect(subject.sum(-5,-7)).to eq(-12)
+  context "#method - subtract" do
+    it "with 2 numbers positive" do
+      expect(subject.subtract(3,2)).to eq(1)
+    end
+  
+    it "with 2 numbers negative" do
+      expect(subject.subtract(-3,-2)).to eq(-1)
+    end
   end
+  
 
-  it "#subtact for 2 numbers positive" do
-    expect(subject.subtract(3,2)).to eq(1)
+  context "#method - multiply" do
+    it "with 2 numbers positive " do
+      expect(subject.multiply(2,2)).to eq(4)
+    end
+  
+    it "with 2 numbers negative " do
+      expect(subject.multiply(-2,-2)).to eq(4)
+    end
   end
+  
 
-  it "#subtact for 2 numbers negative" do
-    expect(subject.subtract(-3,-2)).to eq(-1)
+  context "#method - division" do
+    it "with 2 numbers positive " do
+      expect(subject.division(2,2)).to eq(1)
+    end  
+  
+    it "with 2 numbers negative " do
+      expect(subject.division(-2,-2)).to eq(1)
+    end
+  
+    it "with error divided by 0" do
+      expect(subject.division(2,0)).to eq("Erro: divided by 0")
+    end
   end
-
-  it "#multiply for 2 numbers positive " do
-    expect(subject.multiply(2,2)).to eq(4)
-  end
-
-  it "#multiply for 2 numbers negative " do
-    expect(subject.multiply(-2,-2)).to eq(4)
-  end
-
-  it "#division for 2 numbers positive " do
-    expect(subject.division(2,2)).to eq(1)
-  end  
-
-  it "#division for 2 numbers negative " do
-    expect(subject.division(-2,-2)).to eq(1)
-  end
-
-  it "#division error divided by 0" do
-    expect(subject.division(2,0)).to eq("Erro: divided by 0")
-  end
+  
 
 end

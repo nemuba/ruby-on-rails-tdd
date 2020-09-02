d6cf9620e5762646e75a2f9efea159eb560e2b7e
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-09-02 15:57:17 -0300

    Matchers de Classes

---
+++ entendendo_rspec/spec/matchers/classes/classes_spec.rb
@@ -0,0 +1,21 @@
+require 'string_nao_vazia'
+
+describe "Matchers de Classes" do
+  subject { StringNaoVazia.new } 
+
+  it 'be_instance_of' do
+    expect(subject).to be_instance_of(StringNaoVazia) 
+  end
+
+  it 'be_kind_of' do
+    expect(subject).to be_kind_of(String) 
+  end
+
+  it 'be_an' do
+    expect(subject).to be_an(String) 
+  end
+
+  it 'be_a' do
+    expect(subject).to be_a(StringNaoVazia) 
+  end
+end

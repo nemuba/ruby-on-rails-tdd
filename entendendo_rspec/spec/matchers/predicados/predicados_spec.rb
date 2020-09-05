516cc7bc1def747399e57bcbc8f0263a15e2ed79
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-09-05 20:00:01 -0300

    matchers de  Predicado

---
+++ entendendo_rspec/spec/matchers/predicados/predicados_spec.rb
@@ -0,0 +1,29 @@
+describe "Predicados" do
+  it 'be_odd' do
+    expect(1).to be_odd 
+  end
+
+  it 'be_even' do
+    expect(12).to be_even 
+  end
+
+  it 'be_nil' do
+    expect(nil).to be_nil 
+  end
+
+  it 'be_positive' do
+    expect(1).to be_positive
+  end
+
+  it 'be_negative' do
+    expect(-1).to be_negative
+  end
+
+  it 'be_zero' do
+    expect(1-1).to be_zero
+  end
+
+  it 'be_nonzero' do
+    expect(1-2).to be_nonzero
+  end
+end

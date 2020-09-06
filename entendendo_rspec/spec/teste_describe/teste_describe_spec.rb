50ef3d867a9c3be85169a1ea679b6c1655cf78c2
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-09-05 21:27:34 -0300

    Umpouco mais de RSpec.describe

---
+++ entendendo_rspec/spec/teste_describe/teste_describe_spec.rb
@@ -0,0 +1,11 @@
+describe [1,2] do
+  it 'Array' do
+    expect(subject).to be_kind_of(Array)  
+  end
+end
+
+describe "Alef" do
+  it 'String' do
+    expect(subject).to be_kind_of(String)  
+  end
+end

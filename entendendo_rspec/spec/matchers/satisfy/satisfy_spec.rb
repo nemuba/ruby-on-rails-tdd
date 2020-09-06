a830c5bb5614b1ba4d9db7a9ae9948926ea33770
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-09-05 22:45:32 -0300

    Matchers satisfy

---
+++ entendendo_rspec/spec/matchers/satisfy/satisfy_spec.rb
@@ -0,0 +1,9 @@
+describe 'Matchers satisfy' do
+  it {
+    expect(10).to satisfy('be a multiple of 2') do |x|
+      x.modulo(2) == 0
+    end
+  }
+
+  it { expect(10).to satisfy { |x| x.modulo(2) == 0 } } 
+end

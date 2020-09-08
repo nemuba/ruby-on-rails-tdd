2f7659d908d282ab97d325280052e2ede215d509
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-09-08 08:44:53 -0300

    Negativando Matchers

--- entendendo_rspec/spec/matchers/arrays/arrays_spec.rb
+++ entendendo_rspec/spec/matchers/arrays/arrays_spec.rb
@@ -1,15 +1,12 @@
+RSpec::Matchers.define_negated_matcher :exclude, :include
+
 describe Array.new([1,2,3]), "Array" do
-  it '#include' do
-    expect(subject).to include(1,2)  
-  end
+  it { expect(subject).to include(1,2) }
 
-  it '#contain_exactly' do
-    expect(subject).to contain_exactly(1,2,3)  
-  end
+  it { expect(subject).to exclude(4) } 
 
-  it '#match_array' do
-    expect(subject).to match_array([1,2,3])  
-  end
+  it { expect(subject).to contain_exactly(1,2,3) }
 
-  
+  it { expect(subject).to match_array([1,2,3]) }
+
 end

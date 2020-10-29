5f57ec9e751caf7dbfdb00b81610fb13320ca6c8
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2020-10-29 20:30:49 -0300

    Test Double

---
+++ entendendo_rspec/spec/matchers/teste_doubles/test_double.rb
@@ -0,0 +1,8 @@
+describe 'Test Double' do
+  it 'double("User")' do
+    user = double('User')
+    allow(user).to receive(:name).and_return('Alef')
+    expect(user).to have_attributes(name: a_string_starting_with('A'))
+    expect(user.name).to be_kind_of(String)
+  end
+end

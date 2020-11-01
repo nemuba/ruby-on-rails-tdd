a9f0b277255332b9f16b57099d4d923c6582d6e9
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2020-11-01 18:54:35 -0300

    Create List

--- test_app/spec/models/order_spec.rb
+++ test_app/spec/models/order_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Order, type: :model do
     order = create(:order, customer: customer_default)
     expect(order.customer).to be_kind_of(Customer)
   end
+
+  it '# Customer vip - create three orders' do
+    customer_default = create(:customer_default)
+    orders = create_list(:order, 3, customer: customer_default)
+    expect(orders.count).to eq(3)
+    expect(customer_default.orders.count).to eq(3)
+  end
 end

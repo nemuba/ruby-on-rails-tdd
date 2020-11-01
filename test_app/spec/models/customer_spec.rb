51718e8ef80b51677f35e18745d49cebf6c5152a
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2020-11-01 12:29:40 -0300

    Herança

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -6,9 +6,14 @@ RSpec.describe Customer, type: :model do
     expect(alef.full_name).to start_with('Sr. ')
   end
 
-  it '#full_name' do
-    alef = create(:user, name: 'Alef') # ou create(:customer, name: 'Alef')
-    expect(alef.full_name).to eq('Sr. Alef')
+  it '#factory customer_vip' do
+    alef = create(:customer_vip)
+    expect(alef.vip).to eq(true)
+  end
+
+  it '#factory customer_default' do
+    alef = create(:customer_default)
+    expect(alef.vip).to eq(false)
   end
 
   it { expect { create(:customer) }.to change { Customer.all.count }.by(1) }

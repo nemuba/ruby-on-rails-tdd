dac4e1bef01def58fecea28806921b26c9fcd1da
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2020-11-01 01:01:54 -0300

    override attributes and aliases to factories

--- test_app/spec/factories/customer.rb
+++ test_app/spec/factories/customer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :customer do
+  factory :customer, aliases: [:user] do
     name { Faker::Name.name }
     email { Faker::Internet.email }
   end
--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe Customer, type: :model do
     expect(alef.full_name).to start_with('Sr. ')
   end
 
-  it { expect { create(:customer) }.to change{ Customer.all.count }.by(1) }
+  it '#full_name' do
+    alef = create(:user, name: 'Alef') # ou create(:customer, name: 'Alef')
+    expect(alef.full_name).to eq('Sr. Alef')
+  end
 
+  it { expect { create(:customer) }.to change { Customer.all.count }.by(1) }
 end

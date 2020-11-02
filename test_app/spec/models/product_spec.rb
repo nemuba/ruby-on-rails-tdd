69671956d36e217cd9e4b8e0f41739e22cb4683e
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2020-11-02 01:47:08 -0300

    Shoulda-matchers

--- test_app/spec/models/product_spec.rb
+++ test_app/spec/models/product_spec.rb
@@ -3,26 +3,47 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  it 'is valid with description, price and category' do
-    product = create(:product)
-    expect(product).to be_valid
+  # it 'is valid with description, price and category' do
+  #   product = create(:product)
+  #   expect(product).to be_valid
+  # end
+
+  # it 'is invalid without description' do
+  #   product = build(:product, description: nil)
+  #   product.valid?
+  #   expect(product.errors.full_messages).to include("Description can't be blank")
+  # end
+
+  # it 'is invalid without price' do
+  #   product = build(:product, price: nil)
+  #   product.valid?
+  #   expect(product.errors.full_messages).to include("Price can't be blank")
+  # end
+
+  # it 'is invalid without category' do
+  #   product = build(:product, category: nil)
+  #   product.valid?
+  #   expect(product.errors.full_messages).to include('Category must exist')
+  # end
+  
+  context 'Validations' do
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:price) }
   end
 
-  it 'is invalid without description' do
-    product = build(:product, description: nil)
-    product.valid?
-    expect(product.errors.full_messages.first).to eq("Description can't be blank")
+  context 'Associations' do
+    it { is_expected.to belong_to(:category) }
   end
 
-  it 'is invalid without price' do
-    product = build(:product, price: nil)
-    product.valid?
-    expect(product.errors.full_messages.first).to eq("Price can't be blank")
-  end
-
-  it 'is invalid without category' do
-    product = build(:product, category: nil)
-    product.valid?
-    expect(product.errors.full_messages.first).to eq('Category must exist')
+  context 'Instance method' do
+    it '#valid' do
+      product = create(:product)
+      expect(product).to be_valid
+    end
+    
+    it '#full_description' do
+      product = create(:product)
+      expect(product.full_description).to eq("#{product.description} - #{product.price}")
+    end
   end
 end
--- test_app/spec/rails_helper.rb
+++ test_app/spec/rails_helper.rb
@@ -31,6 +31,15 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+
+  # Shoulda-matchers
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
   # Time Helper
   config.include ActiveSupport::Testing::TimeHelpers
   # Include FactoryBot

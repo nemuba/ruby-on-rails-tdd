4d8e30db87d885c52dc912fcc74a971bf64c583b
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2020-11-01 21:55:49 -0300

    config vcr with rspec

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,9 +1,7 @@
 describe 'HTTParty' do
-  it '#get id = 1' do
-    VCR.use_cassette('jsonplaceholder/posts/1') do
-      response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
-      post = JSON.parse(response.body, simbolize_names: true)
-      expect(post['id']).to eq(1)
-    end
+  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts' } do
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts')
+    posts = JSON.parse(response.body, simbolize_names: true)
+    expect(posts.count).to eq(100)
   end
 end
